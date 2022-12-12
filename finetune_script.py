ccf4f7da9d2452ef2102733c931a1abed9338789
Joschka Braun <joschka.braun@gmail.com>
2022-12-12 15:31:00 +0100

    fix: fix finetune script

--- finetune_script.py
+++ finetune_script.py
@@ -2,19 +2,20 @@ from jina import Client, DocumentArray
 import hubble
 
 # specify the path to the images
-path_to_images = '/Users/joschkabraun/dev/data/briscoe/briscoe4-png-512-cropped'
-# specify the category of the images
-category = 'dog'
+path_to_instance_images = '/path/to/instance/images'
+# specify the category of the images, this could be e.g. 'painting', 'dog', 'bottle', etc.
+category = 'painting'
+# 'private' for training private model from pretrained model, 'meta' for training metamodel
+target_model = 'private'
 
-# use the first host if accessing from outside Berlin office, else use the second one
+# some custom parameters for the training
+max_train_steps = 200
+learning_rate = 1e-4
+
+
 host = 'grpc://87.191.159.105:51111'
-# host = 'grpc://192.168.178.31:51111'
 
-# 'own' for training from pretrained model, 'meta' for training metamodel
-target_model = 'own'
-
-
-docs = DocumentArray.from_files(f'{path_to_images}/**')
+docs = DocumentArray.from_files(f'{path_to_instance_images}/**')
 for doc in docs:
     doc.load_uri_to_blob()
     doc.uri = None
@@ -23,7 +24,6 @@ client = Client(host=host)
 
 identifier_doc = client.post(
     on='/experimental/finetune',
-    # on='/finetune',
     inputs=docs,
     parameters={
         'jwt': {
@@ -31,9 +31,8 @@ identifier_doc = client.post(
         },
         'category': category,
         'target_model': target_model,
-        'num_category_images': 200,
-        # 'learning_rate': 1e-6,
-        # 'max_train_steps': 600,
+        'learning_rate': learning_rate,
+        'max_train_steps': max_train_steps,
     },
 )
 
