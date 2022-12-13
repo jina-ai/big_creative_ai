4cb77aa6fc74512b9ef1b712023a4f87947ad95d
Joschka Braun <joschka.braun@gmail.com>
2022-12-13 13:18:47 +0100

    docs: update docs

--- list_identifiers_script.py
+++ list_identifiers_script.py
@@ -1,12 +1,8 @@
 from jina import Client
 import hubble
 
-# use the first host if accessing from outside Berlin office, else use the second one
-host = 'grpc://87.191.159.105:51111'
-host = 'grpc://192.168.178.31:51111'
 
-
-client = Client(host=host)
+client = Client(host='grpc://87.191.159.105:51111')
 
 identifier_n_categories = client.post(
     on='/list_identifiers_n_categories',
