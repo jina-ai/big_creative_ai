ed18eda15833237419608b1e4a33f6158c22e8a8
Joschka Braun <joschka.braun@gmail.com>
2023-01-26 11:46:19 +0100

    fix: free memory for not colab

--- BIGDreamBooth/executor/scripts/generate.py
+++ BIGDreamBooth/executor/scripts/generate.py
@@ -1,43 +1,47 @@
-import inspect
+import gc
 import os
 
 import torch
-from accelerate import Accelerator
 from diffusers import StableDiffusionPipeline
+from torch.utils.data import Dataset
 from tqdm import tqdm
 
-# add to the sys.path
-import sys
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-parentdir = os.path.dirname(parentdir)
-parentdir = os.path.dirname(parentdir)
-sys.path.insert(0, parentdir)
 
-try:
-    from BIGDreamBooth.executor.dreambooth import PromptDataset
-except ImportError:
-    from ..dreambooth import PromptDataset
+class PromptDataset(Dataset):
+    "A simple dataset to prepare the prompts to generate class images on multiple GPUs."
+    def __init__(self, prompt, num_samples):
+        self.prompt = prompt
+        self.num_samples = num_samples
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, index):
+        example = {}
+        example["prompt"] = self.prompt
+        example["index"] = index
+        return example
 
 
 def _generate(save_dir: str, num_images: int, model_path: str, prompt: str, batch_size: int, revision=None):
-    accelerator = Accelerator()
-
     pipeline = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16, revision=revision)
     pipeline.set_progress_bar_config(disable=True)
 
     sample_dataset = PromptDataset(prompt, num_images)
     sample_dataloader = torch.utils.data.DataLoader(sample_dataset, batch_size=batch_size)
 
-    sample_dataloader = accelerator.prepare(sample_dataloader)
-    pipeline.to(accelerator.device)
+    pipeline.to('cuda' if torch.cuda.is_available() else 'cpu')
 
     cnt = 0
-    for example in tqdm(sample_dataloader, desc="Generating images", disable=not accelerator.is_local_main_process):
+    for example in tqdm(sample_dataloader, desc="Generating images"):
         images = pipeline(example["prompt"]).images
         for image in images:
             image.save(os.path.join(save_dir, f"{cnt}.jpg"), format="JPEG", quality=95)
             cnt += 1
+
+    gc.collect()
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
 
 
 if __name__ == '__main__':
